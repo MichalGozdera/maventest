47696aab5a0bdf6bf1d08b22fdb0ab4d02579fbf
Michal Gozdera <michael.gozdera@gmail.com>
2020-01-12 14:12:54 +0100

    commented test

--- printing/src/test/java/MainTest.java
+++ printing/src/test/java/MainTest.java
@@ -1,33 +1,33 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.util.logging.Logger;
-
-
-import org.junit.Assert;
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
-public class MainTest {
-
-    Logger logger = Logger.getGlobal();
-
-    @Test
-    public void testReadLicense() throws Exception {
-        File folder = new File("./target/generated-resources/license");
-        if (folder == null ||folder.listFiles()==null) {
-            logger.info("license folder does not exists");
-        } else if (folder.listFiles().length > 0) {
-            File[] listOfFiles = folder.listFiles();
-            logger.info(listOfFiles[0].getName());
-            assertEquals("wafel", listOfFiles[0].getName());
-        } else {
-            logger.info("license folder is empty");
-        }
-
-    }
-}
+//import java.io.BufferedWriter;
+//import java.io.File;
+//import java.io.FileInputStream;
+//import java.io.IOException;
+//import java.nio.charset.StandardCharsets;
+//import java.nio.file.Files;
+//import java.util.logging.Logger;
+//
+//
+//import org.junit.Assert;
+//import org.junit.Test;
+//
+//import static org.junit.Assert.*;
+//
+//public class MainTest {
+//
+//    Logger logger = Logger.getGlobal();
+//
+//    @Test
+//    public void testReadLicense() throws Exception {
+//        File folder = new File("./target/generated-resources/license");
+//        if (folder == null ||folder.listFiles()==null) {
+//            logger.info("license folder does not exists");
+//        } else if (folder.listFiles().length > 0) {
+//            File[] listOfFiles = folder.listFiles();
+//            logger.info(listOfFiles[0].getName());
+//            assertEquals("wafel", listOfFiles[0].getName());
+//        } else {
+//            logger.info("license folder is empty");
+//        }
+//
+//    }
+//}
