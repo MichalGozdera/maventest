c47c7c838f13494b8a1cd9f6da4ec2850b51fe20
Michal Gozdera <michael.gozdera@gmail.com>
2020-01-12 17:01:18 +0100

    new test

--- dupa/src/test/java/MainTestDupaTest.java
+++ dupa/src/test/java/MainTestDupaTest.java
@@ -1,6 +1,10 @@
 
 import java.io.File;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.logging.Logger;
+
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -17,7 +21,11 @@ public class MainTestDupaTest {
         } else if (folder.listFiles().length > 0) {
             File[] listOfFiles = folder.listFiles();
             logger.info(listOfFiles[0].getName());
-            assertEquals("wafel", listOfFiles[0].getName());
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            String expirationDateS = StringUtils.isEmpty(listOfFiles[0].getName()) ?
+                    LocalDate.now().plusDays(14).format(formatter) :
+                    listOfFiles[0].getName();
+            logger.info(expirationDateS);
         } else {
             logger.info("dupa license folder is empty");
         }
